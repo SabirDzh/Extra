b4d3f44f3eeb9493870bc998bdb4873429637874
Sabir Dzhabrailov <SabirDzhabrailov.111@mail.ru>
2026-02-07 23:04:35 +0300

    feat(schemas): add course schemas

---
+++ backend/core/schemas/course.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+
+class CourseBase(BaseModel):
+    pass
+
+
+class CourseRead(BaseModel):
+    pass
+
+
+class CourseCreate(BaseModel):
+    pass
+
+
+class CourseUpdate(BaseModel):
+    pass
