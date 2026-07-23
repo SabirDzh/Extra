7e78bb88c7cac35e3de765f8b9d308b3747ccf00
Sabir Dzhabrailov <SabirDzhabrailov.111@gmail.com>
2026-07-23 15:54:57 +0300

    fix(auth): restrict course access for buyer role

--- backend/api/dependencies/authorization.py
+++ backend/api/dependencies/authorization.py
@@ -24,7 +24,6 @@ async def current_course_allowed_user(
         UserRole.seller,
         UserRole.serviceman,
         UserRole.installer,
-        UserRole.buyer,
     }
     if current_user.role not in allowed_roles:
         raise HTTPException(
